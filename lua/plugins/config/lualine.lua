6b638ac62a688ab50fcae4fcc0dcf278ac3fc98a
feyzee <11005389+feyzee@users.noreply.github.com>
2025-01-24 22:28:07 +0530

    filetypes in lualine

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -3,13 +3,13 @@ return {
     theme = 'tokyonight',
     component_separators = '|',
     section_separators = { left = '', right = '' },
-    disabled_filetypes = {'mason','lazy','NvimTree'},
+    disabled_filetypes = {'mason', 'lazy', 'NvimTree', 'neo-tree'},
   },
   sections = {
     lualine_a = {
       { 'mode', separator = { left = '' }, right_padding = 2 },
     },
-    lualine_b = { 'filename', 'branch' },
+    lualine_b = { {'filename', path = 1}, {'branch'} },
     lualine_c = { 'fileformat' },
     lualine_x = {},
     lualine_y = { 'filetype', 'progress' },
