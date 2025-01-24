a36bba70ffaee824418cad7912728a2653f89a29
feyzee <11005389+feyzee@users.noreply.github.com>
2025-01-24 22:28:41 +0530

    updates lspconfig

--- lua/core/lspconfig.lua
+++ lua/core/lspconfig.lua
@@ -105,6 +105,7 @@ local servers = {
     "gopls",
     "lua_ls",
     "pylsp",
+    -- "python-lsp-server",
     "terraformls",
     "tflint",
     "yamlls",
@@ -117,6 +118,16 @@ for _, lsp in ipairs(servers) do
 	})
 end
 
+lspconfig.helm_ls.setup {
+  settings = {
+    ['helm-ls'] = {
+      yamlls = {
+        path = "yaml-language-server",
+      }
+    }
+  }
+}
+
 lspconfig.yamlls.setup({
   settings = {
     yaml = {
@@ -125,6 +136,13 @@ lspconfig.yamlls.setup({
     }
   }
 })
+
+-- if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
+--   vim.diagnostic.disable(bufnr)
+--   vim.defer_fn(function()
+--     vim.diagnostic.reset(nil, bufnr)
+--   end, 1000)
+-- end
 
 -------------------
 
@@ -146,3 +164,32 @@ lspconfig.yamlls.setup({
 --     },
 --   },
 -- }
+--
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+--   callback = function(ev)
+--     -- Enable completion triggered by <c-x><c-o>
+--     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+--
+--     -- Buffer local mappings.
+--     -- See `:help vim.lsp.*` for documentation on any of the below functions
+--     local opts = { buffer = ev.buf }
+--     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+--     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+--     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+--     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+--     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+--     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+--     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+--     vim.keymap.set('n', '<space>wl', function()
+--       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--     end, opts)
+--     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+--     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+--     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+--     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+--     vim.keymap.set('n', '<space>f', function()
+--       vim.lsp.buf.format { async = true }
+--     end, opts)
+--   end,
+-- })
