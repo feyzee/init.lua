605349cf3eb39a09aaee8cbdec757a4cbd049b8c
feyzee <11005389+feyzee@users.noreply.github.com>
2025-01-28 21:27:20 +0530

    added config for arrow.nvim

---
+++ lua/plugins/config/arrow.lua
@@ -0,0 +1,5 @@
+return {
+  show_icons = true,
+  leader_key = ';', -- Recommended to be a single key
+  buffer_leader_key = 'm', -- Per Buffer Mappings
+}
--- lua/plugins/lazy_plugins.lua
+++ lua/plugins/lazy_plugins.lua
@@ -62,17 +62,7 @@ return {
   "someone-stole-my-name/yaml-companion.nvim",
 	"nvim-neorg/neorg",
 	"kevinhwang91/nvim-bqf",
-  {
-    "otavioschwanck/arrow.nvim",
-    -- dependencies = {
-    --   { "nvim-tree/nvim-web-devicons" },
-    -- },
-    opts = {
-      show_icons = true,
-      leader_key = ';', -- Recommended to be a single key
-      buffer_leader_key = 'm', -- Per Buffer Mappings
-    }
-  },
+  "otavioschwanck/arrow.nvim",
   {
     "folke/trouble.nvim",
     opts = {}, -- for default options, refer to the configuration section for custom setup.
