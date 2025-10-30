e7ee660eb17f4c0e6ab658aefd0932a68c0b8b5b
feyzee <11005389+feyzee@users.noreply.github.com>
2025-10-30 13:48:28 +0530

    feat(expermental): new lspconfig

--- lua/core/lspconfig.lua
+++ lua/core/lspconfig.lua
@@ -100,18 +100,19 @@ end
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local lspconfig = require("lspconfig")
+-- Enable language servers with the additional completion capabilities offered by nvim-cmp
+-- local lspconfig = require("lspconfig")
+local lspconfig = vim.lsp.config
 local servers = {
-    "ts_ls",
-    "bashls",
-    "dockerls",
-    "gopls",
-    "lua_ls",
-    "pylsp",
-    "terraformls",
-    "tflint",
-    "yamlls",
+  "ts_ls",
+  "bashls",
+  "dockerls",
+  "gopls",
+  "lua_ls",
+  "pylsp",
+  "terraformls",
+  "tflint",
+  "yamlls",
 }
 
 for _, lsp in ipairs(servers) do
@@ -121,78 +122,24 @@ for _, lsp in ipairs(servers) do
 	})
 end
 
-lspconfig.helm_ls.setup {
-  settings = {
-    ['helm-ls'] = {
-      yamlls = {
-        path = "yaml-language-server",
-      }
-    }
-  }
-}
+vim.lsp.enable(servers)
 
-lspconfig.yamlls.setup({
-  settings = {
-    yaml = {
-      -- FIX mapKeyOrder warning
-      keyOrdering = false
-    }
-  }
-})
-
--- if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
---   vim.diagnostic.disable(bufnr)
---   vim.defer_fn(function()
---     vim.diagnostic.reset(nil, bufnr)
---   end, 1000)
--- end
-
--------------------
-
--- lspconfig.lua_ls.setup {
---   lua_lsp_settings = {
---     Lua = {
---       diagnostics = {
---         globals = { "vim" },
---       },
---       workspace = {
---         library = {
---           [vim.fn.expand "$VIMRUNTIME/lua"] = true,
---           [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
---           [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
---         },
---         maxPreload = 100000,
---         preloadFileSize = 10000,
---       },
---     },
---   },
+-- lspconfig.helm_ls.setup {
+--   settings = {
+--     ['helm-ls'] = {
+--       yamlls = {
+--         path = "yaml-language-server",
+--       }
+--     }
+--   }
 -- }
---
--- vim.api.nvim_create_autocmd('LspAttach', {
---   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
---   callback = function(ev)
---     -- Enable completion triggered by <c-x><c-o>
---     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
---
---     -- Buffer local mappings.
---     -- See `:help vim.lsp.*` for documentation on any of the below functions
---     local opts = { buffer = ev.buf }
---     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
---     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
---     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
---     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
---     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
---     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
---     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
---     vim.keymap.set('n', '<space>wl', function()
---       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---     end, opts)
---     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
---     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
---     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
---     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
---     vim.keymap.set('n', '<space>f', function()
---       vim.lsp.buf.format { async = true }
---     end, opts)
---   end,
+
+
+-- lspconfig.yamlls.setup({
+--   settings = {
+--     yaml = {
+--       -- FIX mapKeyOrder warning
+--       keyOrdering = false
+--     }
+--   }
 -- })
