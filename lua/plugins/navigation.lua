896e13e2489cd8cb7595d294c197ab310f0e4265
feyzee <11005389+feyzee@users.noreply.github.com>
2025-11-11 15:37:33 +0530

    feat: flash.nvim

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -7,4 +7,31 @@ return {
     "phaazon/hop.nvim",
     opts = {}
   },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "\\",         mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+      { "<leader>\\", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "<c-s>",      mode = { "c" },           function() require("flash").toggle() end,     desc = "Toggle Flash Search" },
+      -- Simulate nvim-treesitter incremental selection
+      {
+        "<c-space>",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter({
+            actions = {
+              ["<c-space>"] = "next",
+              ["<BS>"] = "prev"
+            }
+          })
+        end,
+        desc = "Treesitter Incremental Selection"
+      },
+    },
+  },
 }
