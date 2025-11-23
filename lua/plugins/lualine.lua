8e1ed175b160e952b28a9f315a9acfd12d267d1d
feyzee <11005389+feyzee@users.noreply.github.com>
2025-11-23 21:08:27 +0530

    feat: better lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -16,7 +16,7 @@ return {
         { "mode", separator = { left = "" }, right_padding = 2 },
       },
 
-      lualine_b = { { "filename", path = 1 }, { "branch" } },
+      lualine_b = { { "branch" } },
       lualine_c = { "diagnostics", "diff" },
       lualine_x = { "selectioncount", "encoding", "fileformat" },
       lualine_y = {
@@ -33,7 +33,6 @@ return {
             end
             return table.concat(buf_client_names, ", ")
           end,
-          -- icon = " ",
         },
         "progress",
       },
@@ -45,19 +44,32 @@ return {
 
     inactive_sections = {
       lualine_a = { { "filename", file_status = true, path = 1 } },
-      lualine_b = {},
+      lualine_b = { "diagnostics", "diff" },
       lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = { { "branch" }, { "location" } },
+      lualine_x = { { "branch" }, { "fileformat" } },
+      lualine_y = { -- Displays the attached LSPs
+        function()
+          local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
+          if #buf_clients == 0 then
+            return "No LSP"
+          end
+          local buf_client_names = {}
+          for _, client in pairs(buf_clients) do
+            table.insert(buf_client_names, client.name)
+          end
+          return table.concat(buf_client_names, ", ")
+        end,
+      },
+      lualine_z = { "location" },
     },
 
     tabline = {
       lualine_a = {
         {
           "tabs",
-          mode = 1,
+          mode = 0, -- 0: Shows tab_name or new tab count if no name is set
           path = 0,
+          max_length = vim.o.columns, -- Force full width to avoid truncation
           fmt = function(name, context)
             local tabnr = context.tabnr
             local tab_name = vim.fn.gettabvar(tabnr, "tab_name")
@@ -84,7 +96,9 @@ return {
       lualine_c = {},
       lualine_x = {},
       lualine_y = {},
-      lualine_z = {},
+      lualine_z = {
+        { "filename", path = 1 }, -- Show relative path of current buffer
+      },
     },
     extensions = {},
   },
