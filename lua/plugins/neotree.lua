d7e4c9ad94296eab21f6a36c88d9b196f0e8eeec
feyzee <11005389+feyzee@users.noreply.github.com>
2025-11-23 10:26:06 +0530

    fix(ai): neotree uses mini.icons

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,6 +1,13 @@
+-- Included plugins in this module:
+--   - neo-tree.nvim
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
+
+  dependencies = {
+    { "echasnovski/mini.icons" },
+  },
 
   opts = {
     close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
@@ -41,6 +48,27 @@ return {
         expander_highlight = "NeoTreeExpander",
       },
       icon = {
+        provider = function(icon, node) -- setup a custom icon provider
+          local text, hl
+          local mini_icons = require("mini.icons")
+          if node.type == "file" then          -- if it's a file, set the text/hl
+            text, hl = mini_icons.get("file", node.name)
+          elseif node.type == "directory" then -- get directory icons
+            text, hl = mini_icons.get("directory", node.name)
+            -- only set the icon text if it is not expanded
+            if node:is_expanded() then
+              text = nil
+            end
+          end
+
+          -- set the icon text/highlight only if it exists
+          if text then
+            icon.text = text
+          end
+          if hl then
+            icon.highlight = hl
+          end
+        end,
         folder_closed = "",
         folder_open = "",
         folder_empty = "󰜌",
