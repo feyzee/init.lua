697537a95c9df35b464caaf5c1dda799b0393c90
feyzee <11005389+feyzee@users.noreply.github.com>
2025-11-23 17:02:00 +0530

    fix: blink

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,243 +4,242 @@
 --   - friendly-snippets
 
 local completion_kinds = { -- NOTE: remove if not needed
-  Array = " ",
-  Boolean = "󰨙 ",
-  Class = " ",
-  Codeium = "󰘦 ",
-  Color = " ",
-  Control = " ",
-  Collapsed = " ",
-  Constant = "󰏿 ",
-  Constructor = " ",
-  Copilot = " ",
-  Enum = " ",
-  EnumMember = " ",
-  Event = " ",
-  Field = " ",
-  File = " ",
-  Folder = " ",
-  Function = "󰊕 ",
-  Interface = " ",
-  Key = " ",
-  Keyword = " ",
-  Method = "󰊕 ",
-  Module = " ",
-  Namespace = "󰦮 ",
-  Null = " ",
-  Number = "󰎠 ",
-  Object = " ",
-  Operator = " ",
-  Package = " ",
-  Property = " ",
-  Reference = " ",
-  Snippet = "󱄽 ",
-  String = " ",
-  Struct = "󰆼 ",
-  Supermaven = " ",
-  TabNine = "󰏚 ",
-  Text = " ",
-  TypeParameter = " ",
-  Unit = " ",
-  Value = " ",
-  Variable = "󰀫 ",
+	Array = " ",
+	Boolean = "󰨙 ",
+	Class = " ",
+	Codeium = "󰘦 ",
+	Color = " ",
+	Control = " ",
+	Collapsed = " ",
+	Constant = "󰏿 ",
+	Constructor = " ",
+	Copilot = " ",
+	Enum = " ",
+	EnumMember = " ",
+	Event = " ",
+	Field = " ",
+	File = " ",
+	Folder = " ",
+	Function = "󰊕 ",
+	Interface = " ",
+	Key = " ",
+	Keyword = " ",
+	Method = "󰊕 ",
+	Module = " ",
+	Namespace = "󰦮 ",
+	Null = " ",
+	Number = "󰎠 ",
+	Object = " ",
+	Operator = " ",
+	Package = " ",
+	Property = " ",
+	Reference = " ",
+	Snippet = "󱄽 ",
+	String = " ",
+	Struct = "󰆼 ",
+	Supermaven = " ",
+	TabNine = "󰏚 ",
+	Text = " ",
+	TypeParameter = " ",
+	Unit = " ",
+	Value = " ",
+	Variable = "󰀫 ",
 }
 
 return {
-  "saghen/blink.cmp",
-  version = "1.*",
+	"saghen/blink.cmp",
+	version = "1.*",
 
-  opts_extend = {
-    "sources.completion.enabled_providers",
-    "sources.compat",
-    "sources.default",
-  },
+	opts_extend = {
+		"sources.completion.enabled_providers",
+		"sources.compat",
+		"sources.default",
+	},
 
-  dependencies = {
-    {
-      "saghen/blink.compat",
-      optional = true, -- make optional so it's only enabled if any extras need it
-      opts = {},
-      version = not vim.g.lazyvim_blink_main and "*",
-    },
-    { "rafamadriz/friendly-snippets" },
-  },
+	dependencies = {
+		{
+			"saghen/blink.compat",
+			optional = true, -- make optional so it's only enabled if any extras need it
+			opts = {},
+			version = not vim.g.lazyvim_blink_main and "*",
+		},
+		{ "rafamadriz/friendly-snippets" },
+	},
 
-  event = { "InsertEnter", "CmdlineEnter" },
+	event = { "InsertEnter", "CmdlineEnter" },
 
-  opts = {
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "mono",
-      kind_icons = completion_kinds,
-    },
+	opts = {
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+			kind_icons = completion_kinds,
+		},
 
-    cmdline = {
-      keymap = { preset = 'inherit' },
-      completion = { menu = { auto_show = true, min_keyword_length = 5 } },
-    },
+		cmdline = {
+			keymap = { preset = "inherit" },
+			completion = { menu = { auto_show = true } },
+		},
 
-    completion = {
-      accept = { auto_brackets = { enabled = true } },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 500,
-        treesitter_highlighting = true,
-        update_delay_ms = 50,
+		completion = {
+			accept = { auto_brackets = { enabled = true } },
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 500,
+				treesitter_highlighting = true,
+				update_delay_ms = 50,
 
-        window = {
-          border = "rounded",
-          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
-        },
-      },
+				window = {
+					border = "rounded",
+					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+				},
+			},
 
-      ghost_text = { enabled = true },
-      keyword = { range = "full" },
-      list = {
-        max_items = 50,
-        selection = { preselect = true, auto_insert = false },
-      },
+			ghost_text = { enabled = true },
+			keyword = { range = "full" },
+			list = {
+				max_items = 50,
+				selection = { preselect = true, auto_insert = false },
+			},
 
-      menu = {
-        auto_show = true,
-        border = "rounded",
-        min_width = 25,
-        max_height = 20,
-        winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+			menu = {
+				auto_show = true,
+				border = "rounded",
+				min_width = 25,
+				max_height = 20,
+				winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
 
-        -- nvim-cmp style menu
-        draw = {
-          columns = {
-            { "label",     "label_description", gap = 1 },
-            { "kind_icon", "kind", gap = 1 },
-          },
+				draw = {
+					columns = {
+						{ "label", "label_description", gap = 1 },
+						{ "kind_icon", "kind", gap = 1 },
+					},
 
-          components = {
-            kind_icon = {
-              text = function(ctx)
-                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-                return kind_icon
-              end,
-              -- (optional) use highlights from mini.icons
-              highlight = function(ctx)
-                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                return hl
-              end,
-            },
-            kind = {
-              -- (optional) use highlights from mini.icons
-              highlight = function(ctx)
-                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                return hl
-              end,
-            },
-          },
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+								return kind_icon
+							end,
+							-- (optional) use highlights from mini.icons
+							highlight = function(ctx)
+								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+								return hl
+							end,
+						},
+						kind = {
+							-- (optional) use highlights from mini.icons
+							highlight = function(ctx)
+								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+								return hl
+							end,
+						},
+					},
 
-          gap = 2,
-          padding = 1,
-          snippet_indicator = "~",
-          treesitter = { "lsp" },
-        },
-      },
+					gap = 2,
+					padding = 1,
+					snippet_indicator = "~",
+					treesitter = { "lsp" },
+				},
+			},
 
-      trigger = { show_in_snippet = false },
-    },
+			trigger = { show_in_snippet = false },
+		},
 
-    fuzzy = { implementation = "prefer_rust_with_warning" },
-    keymap = {
-      preset = "enter",
-      -- TODO: move below config to keymaps.lua
-      ["<A-]>"] = { "snippet_forward", "fallback" },
-      ["<A-[>"] = { "snippet_backward", "fallback" },
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+		keymap = {
+			preset = "enter",
+			-- TODO: move below config to keymaps.lua
+			["<A-]>"] = { "snippet_forward", "fallback" },
+			["<A-[>"] = { "snippet_backward", "fallback" },
 
-      ["<Tab>"] = { "select_next", "fallback" },
-      ["<S-Tab>"] = { "select_prev", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = { "select_next", "fallback" },
+			["<S-Tab>"] = { "select_prev", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
 
-      ["<S-k>"] = { "scroll_documentation_up", "fallback" },
-      ["<S-j>"] = { "scroll_documentation_down", "fallback" },
+			["<S-k>"] = { "scroll_documentation_up", "fallback" },
+			["<S-j>"] = { "scroll_documentation_down", "fallback" },
 
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide", "fallback" },
-      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-    },
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+		},
 
-    signature = {
-      enabled = true,
-      window = { border = 'rounded', show_documentation = false },
-    },
+		signature = {
+			enabled = true,
+			window = { border = "rounded", show_documentation = false },
+		},
 
-    snippets = { preset = "luasnip" },
-    sources = {
-      default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-      min_keyword_length = 2,
+		snippets = { preset = "luasnip" },
+		sources = {
+			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+			min_keyword_length = 2,
 
-      per_filetype = {
-        lua = { inherit_defaults = true, "lazydev" },
-      },
+			per_filetype = {
+				lua = { inherit_defaults = true, "lazydev" },
+			},
 
-      providers = {
-        buffer = {
-          name = "Buffer",
-          enabled = true,
-          max_items = 5,
-          min_keyword_length = 4,
-          module = "blink.cmp.sources.buffer",
-          score_offset = 15, -- the higher the number, the higher the priority
-        },
+			providers = {
+				buffer = {
+					name = "Buffer",
+					enabled = true,
+					max_items = 5,
+					min_keyword_length = 4,
+					module = "blink.cmp.sources.buffer",
+					score_offset = 15, -- the higher the number, the higher the priority
+				},
 
-        lazydev = {
-          name = "LazyDev",
-          module = "lazydev.integrations.blink",
-          score_offset = 101,
-        },
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					score_offset = 101,
+				},
 
-        lsp = {
-          name = "lsp",
-          enabled = true,
-          module = "blink.cmp.sources.lsp",
-          max_items = 15,
-          score_offset = 100,
+				lsp = {
+					name = "lsp",
+					enabled = true,
+					module = "blink.cmp.sources.lsp",
+					max_items = 15,
+					score_offset = 100,
 
-          opts = {
-            show_signature_help = true,
-            resolve_timeout_ms = 100,
-          },
-        },
+					opts = {
+						show_signature_help = true,
+						resolve_timeout_ms = 100,
+					},
+				},
 
-        path = {
-          name = "Path",
-          module = "blink.cmp.sources.path",
-          score_offset = 25,
-          fallbacks = { "snippets", "buffer" },
-          max_items = 5,
+				path = {
+					name = "Path",
+					module = "blink.cmp.sources.path",
+					score_offset = 25,
+					fallbacks = { "snippets", "buffer" },
+					max_items = 5,
 
-          opts = {
-            trailing_slash = false,
-            label_trailing_slash = true,
-            ignore_root_slash = false,
-            get_cwd = function(context)
-              return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-            end,
-            show_hidden_files_by_default = true,
-          },
-        },
+					opts = {
+						trailing_slash = false,
+						label_trailing_slash = true,
+						ignore_root_slash = false,
+						get_cwd = function(context)
+							return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+						end,
+						show_hidden_files_by_default = true,
+					},
+				},
 
-        snippets = {
-          name = "snippets",
-          enabled = true,
-          max_items = 7,
-          module = "blink.cmp.sources.snippets",
-          score_offset = 75,
+				snippets = {
+					name = "snippets",
+					enabled = true,
+					max_items = 7,
+					module = "blink.cmp.sources.snippets",
+					score_offset = 75,
 
-          opts = {
-            use_show_condition = true,
-            show_autosnippets = true,
-            prefer_doc_trig = false,
-            use_label_description = false,
-          },
-        },
-      },
-    },
-  },
+					opts = {
+						use_show_condition = true,
+						show_autosnippets = true,
+						prefer_doc_trig = false,
+						use_label_description = false,
+					},
+				},
+			},
+		},
+	},
 }
