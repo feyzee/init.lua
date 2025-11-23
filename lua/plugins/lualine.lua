3ed619dde1323345882d7a4389e864491204436d
feyzee <11005389+feyzee@users.noreply.github.com>
2025-11-23 17:02:31 +0530

    fix: tabline via lualine disabled

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,64 +2,91 @@
 --   - lualine.nvim
 
 return {
-  "nvim-lualine/lualine.nvim",
-  opts = {
-    options = {
-      theme = "tokyonight",
-      component_separators = "|",
-      section_separators = { left = "", right = "" },
-      disabled_filetypes = { "mason", "lazy", "NvimTree", "neo-tree" },
-    },
-    sections = {
-      lualine_a = {
-        -- { "mode", right_padding = 2 },
-        { "mode", separator = { left = "" }, right_padding = 2 },
-      },
+	"nvim-lualine/lualine.nvim",
+	opts = {
+		options = {
+			theme = "tokyonight",
+			component_separators = "|",
+			section_separators = { left = "", right = "" },
+			disabled_filetypes = { "mason", "lazy", "NvimTree", "neo-tree" },
+		},
+		sections = {
+			lualine_a = {
+				-- { "mode", right_padding = 2 },
+				{ "mode", separator = { left = "" }, right_padding = 2 },
+			},
 
-      lualine_b = { { "filename", path = 1 }, { "branch" } },
-      lualine_c = { "diagnostics", "diff" },
-      lualine_x = { "selectioncount", "encoding", "fileformat" },
-      lualine_y = {
-        "filetype",
-        { -- Displays the attached LSPs
-          function()
-            local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
-            if #buf_clients == 0 then
-              return "No LSP"
-            end
-            local buf_client_names = {}
-            for _, client in pairs(buf_clients) do
-              table.insert(buf_client_names, client.name)
-            end
-            return table.concat(buf_client_names, ", ")
-          end,
-          -- icon = " ",
-        },
-        "progress"
-      },
+			lualine_b = { { "filename", path = 1 }, { "branch" } },
+			lualine_c = { "diagnostics", "diff" },
+			lualine_x = { "selectioncount", "encoding", "fileformat" },
+			lualine_y = {
+				"filetype",
+				{ -- Displays the attached LSPs
+					function()
+						local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
+						if #buf_clients == 0 then
+							return "No LSP"
+						end
+						local buf_client_names = {}
+						for _, client in pairs(buf_clients) do
+							table.insert(buf_client_names, client.name)
+						end
+						return table.concat(buf_client_names, ", ")
+					end,
+					-- icon = " ",
+				},
+				"progress",
+			},
 
-      lualine_z = {
-        { "location", separator = { right = "" }, left_padding = 2 },
-      },
-    },
+			lualine_z = {
+				{ "location", separator = { right = "" }, left_padding = 2 },
+			},
+		},
 
-    inactive_sections = {
-      lualine_a = { { "filename", file_status = true, path = 1 } },
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = { { "branch" }, { "location" } },
-    },
+		inactive_sections = {
+			lualine_a = { { "filename", file_status = true, path = 1 } },
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = { { "branch" }, { "location" } },
+		},
 
-    tabline = {
-      -- lualine_a = {},
-      -- lualine_b = { "branch" },
-      -- lualine_c = { "filename" },
-      -- lualine_x = {},
-      -- lualine_y = {},
-      -- lualine_z = {}
-    },
-    extensions = {},
-  },
+		-- tabline = {
+		-- 	lualine_a = {
+		-- 		{
+		-- 			"tabs",
+		-- 			mode = 1,
+		-- 			path = 0,
+		-- 			fmt = function(name, context)
+		-- 				local tabnr = context.tabnr
+		-- 				local tab_name = vim.fn.gettabvar(tabnr, "tab_name")
+		-- 				if tab_name == "" then
+		-- 					tab_name = nil
+		-- 				end
+		--
+		-- 				local buflist = vim.fn.tabpagebuflist(tabnr)
+		-- 				local unique_bufs = {}
+		-- 				for _, b in ipairs(buflist) do
+		-- 					unique_bufs[b] = true
+		-- 				end
+		-- 				local count = 0
+		-- 				for _ in pairs(unique_bufs) do
+		-- 					count = count + 1
+		-- 				end
+		--
+		-- 				local display_name = tab_name or name
+		-- 				return string.format("%d: %s (%d)", tabnr, display_name, count)
+		-- 			end,
+		-- 		},
+		-- 	},
+		-- 	lualine_b = {},
+		-- 	lualine_c = {},
+		-- 	lualine_x = {},
+		-- 	lualine_y = {},
+		-- 	lualine_z = {},
+		-- 	5,
+		-- },
+		extensions = {},
+	},
 }
